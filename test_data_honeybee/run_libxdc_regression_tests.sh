f6c6afd93561f4bf64ae60e41441582045c064bf
Sergej Schumilo <sergej@schumilo.de>
2021-03-23 17:16:06 +0100

    fix run_libxdc_regression_tests.sh

--- test_data_honeybee/run_libxdc_regression_tests.sh
+++ test_data_honeybee/run_libxdc_regression_tests.sh
@@ -1,25 +1,25 @@
 
-../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_1.pt 2b28981b68229d82 simple
-../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_2_1.pt 2b28981b68229d82 simple
-../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_2_2.pt 997479f7eb30114 simple
-../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_2_3.pt 997479f7eb30114 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_1.pt 2b28981b68229d82 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_2_1.pt 2b28981b68229d82 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_2_2.pt 997479f7eb30114 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 small/small ./honeybee_unittest_data/contrived_small/trace_2_3.pt 997479f7eb30114 simple
 
-../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2.pt 997479f7eb30114 simple
-../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_1.pt 6aea0c1173daaa8d simple
-../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_2.pt 997479f7eb30114 simple
-../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_3.pt 997479f7eb30114 simple
-../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_4.pt 997479f7eb30114 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2.pt 997479f7eb30114 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_1.pt 6aea0c1173daaa8d simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_2.pt 997479f7eb30114 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_3.pt 997479f7eb30114 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x402000 medium/medium ./honeybee_unittest_data/contrived_medium/trace_2_4.pt 997479f7eb30114 simple
 
-../libxdc/build/tester 0x55555555d000 0x5555555a4000 tar/tar ./honeybee_unittest_data/tar/decompress_clion.pt bdfe9a57d4ccfa84 simple
-../libxdc/build/tester 0x55555555d000 0x5555555a4000 tar/tar ./honeybee_unittest_data/tar/help_page.pt 3b3090c2d6a45132 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x55555555d000 0x5555555a4000 tar/tar ./honeybee_unittest_data/tar/decompress_clion.pt bdfe9a57d4ccfa84 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x55555555d000 0x5555555a4000 tar/tar ./honeybee_unittest_data/tar/help_page.pt 3b3090c2d6a45132 simple
 
-../libxdc/build/tester 0x555555558000 0x55555555C000 html_fast_parse/html_fast_parse ./honeybee_unittest_data/html_fast_parse/6_txt.pt 9f76b3608afc750 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x555555558000 0x55555555C000 html_fast_parse/html_fast_parse ./honeybee_unittest_data/html_fast_parse/6_txt.pt 9f76b3608afc750 simple
 
-../libxdc/build/tester 0x55555555e000 0x5555555c2000 ssh/ssh ./honeybee_unittest_data/ssh/interactive_login_attempt_overflow.pt c846461c6e50bfa5 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x55555555e000 0x5555555c2000 ssh/ssh ./honeybee_unittest_data/ssh/interactive_login_attempt_overflow.pt c846461c6e50bfa5 simple
 
-../libxdc/build/tester 0x400000 0x20cf000 clang/clang ./honeybee_unittest_data/clang/compile_simple_c_1.pt abaf8c57a7521c4f simple
-../libxdc/build/tester 0x400000 0x20cf000 clang/clang ./honeybee_unittest_data/clang/compile_simple_c_2.pt 83caa9ab475c39ba simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x400000 0x20cf000 clang/clang ./honeybee_unittest_data/clang/compile_simple_c_1.pt abaf8c57a7521c4f simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x400000 0x20cf000 clang/clang ./honeybee_unittest_data/clang/compile_simple_c_2.pt 83caa9ab475c39ba simple
 
-../libxdc/build/tester 0x401000 0x67e000 mirror/mirror ./honeybee_unittest_data/honey_mirror_1/clang_huge.pt fe0dfc9d784c0cc0 simple
-../libxdc/build/tester 0x401000 0x67e000 mirror/mirror ./honeybee_unittest_data/honey_mirror_1/bash.pt 793a7b749aad7dba simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x67e000 mirror/mirror ./honeybee_unittest_data/honey_mirror_1/clang_huge.pt fe0dfc9d784c0cc0 simple
+LD_LIBRARY_PATH=../libxdc/build/ ../libxdc/build/tester 0x401000 0x67e000 mirror/mirror ./honeybee_unittest_data/honey_mirror_1/bash.pt 793a7b749aad7dba simple
 
